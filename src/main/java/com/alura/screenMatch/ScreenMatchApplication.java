b059ef2c6b0659d212f6784efd19fd0c4b297fdf
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-12 14:57:59 -0300

    primeiro commit

---
+++ src/main/java/com/alura/screenMatch/ScreenMatchApplication.java
@@ -0,0 +1,17 @@
+package com.alura.screenMatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
